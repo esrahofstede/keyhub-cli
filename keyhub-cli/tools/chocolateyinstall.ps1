49eede36f437cac4e35754a13c157d268675441d
Chocolatey <chocolatey@esrahofstede.nl>
2022-04-25 07:27:16 +0000

    AU: 1 updated - keyhub-cli [skip ci] https://gist.github.com/844a26c02e792e3ba134041b365a7ec4/aad49b9772d345499c38b5e702a4b46e50a800e6

--- keyhub-cli/tools/chocolateyinstall.ps1
+++ keyhub-cli/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  url           = 'https://files.topicus-keyhub.com/manual/keyhub-cli-19.3.zip'
-  checksum = '223E842013173C61305D924A8C90FAAC2844E642E17F6940AF4B48F120446531'
+  url           = 'https://files.topicus-keyhub.com/manual/keyhub-cli-20.0.zip'
+  checksum = '71ED31235A516E929B98ED5419A9F70A19B972568313E8E483DDCBABA0BB477D'
   checksumType = 'sha256'
 }
 
